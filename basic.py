43b239ebdc0a632585e165ea7d3de57a1059a3a7
Akshta <87593122+AkshtaMahoday@users.noreply.github.com>
2021-10-06 01:56:42 -0700

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,26 @@
+#python allows multiple assignments
+x=y=z=a=abc=pqr=10;
+print(abc);
+print(x);
+print(pqr);
+
+#single valued variable
+x=2;
+y="task";
+z=2.3;
+
+#List
+x=[10,20,30,40,50,60,70,80,90];
+print(x);
+
+#how to fetch elements from a list
+abc=[10,20,30,40,50,60,70,80,"chocolate","names",90,120,130,140,150,160];
+print(abc[0]);
+print(abc[4]);
+print(abc[3:8]); # when you use range operator it will fetch from (start index: end index -1th index)
+print(abc[9:]);
+print(abc[:7]);
+print(abc[0]);
+print(abc[-1]);
+print(abc[-2])
+
