2e0b627ac6986c4bef49ad009dc32d2a5f77828c
Raghav <91579152+Unferno@users.noreply.github.com>
2022-10-19 19:51:36 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -8,12 +8,32 @@ print(pqr);
 x=2;
 y="task";
 z=2.3;
+m=3
+
 
 #List
 x=[10,20,30,40,50,60,70,80,90];
 print(x);
 
 #how to fetch elements from a list
+abc=[10,20,30,40,50,60,70,80,"chocolate","names",90,120,130,140,150,160];
+print(abc[0]);
+print(abc[4]);
+print(abc[3:8]); # when you use range operator it will fetch from (start index: end index -1th index)
+print(abc[9:]);
+print(abc[:7]);
+print(abc[0]);
+print(abc[-1]);
+print(abc[-2])
+abc=[10,20,30,40,50,60,70,80,"chocolate","names",90,120,130,140,150,160];
+print(abc[0]);
+print(abc[4]);
+print(abc[3:8]); # when you use range operator it will fetch from (start index: end index -1th index)
+print(abc[9:]);
+print(abc[:7]);
+print(abc[0]);
+print(abc[-1]);
+print(abc[-2])
 abc=[10,20,30,40,50,60,70,80,"chocolate","names",90,120,130,140,150,160];
 print(abc[0]);
 print(abc[4]);
